4df3ca30d217159df8e7964c747ff0e11058ba71
shaker <hydrashaker@gmail.com>
2024-11-29 17:39:51 +0530

    main import solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
-import Main from './components/Main/main'
+import Main from './components/Main/Main';
+
 
 function App() {
   return (
